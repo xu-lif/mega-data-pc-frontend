4451fc74d7cf892e2e6837be2bd2ee4fe8fc5824
许立峰 <1554528002@qq.com>
2022-07-27 00:31:41 +0800

    暂存代码

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,31 +1,83 @@
 import styles from "./index.less";
-import { Map, Marker, NavigationControl, InfoWindow } from "react-bmapgl";
+import { Map, MapvglView, MapvglLayer,Marker, Arc, NavigationControl, InfoWindow } from "react-bmapgl";
 import { useRef } from "react";
 import { useEffect } from "react";
+import ConfigJson from './map_styles.json'
+
+const { mapvgl, BMapGL } = window 
+console.log('mapvgl', mapvgl, BMapGL)
+
+const THREE = mapvgl.THREE
 
 const Home = () => {
   const mapRef = useRef(null);
+  const mapvglView = useRef(null)
+  const threeLayer = useRef(null)
+  useEffect(() => {
+  //   var view = new mapvgl.View({
+  //     map: mapRef.current
+  // });
+  
+  // var threeLayer = new mapvgl.ThreeLayer();
+  // view.addLayer(threeLayer);
+  console.log('mapvglView.current', mapvglView.current)
+  // mapvglView.current.addLayer(threeLayer.current)
+
+//   var projection = mapvgl.MercatorProjection;
+// var point = projection.convertLL2MC(new BMapGL.Point(116.403928,39.914972));
+// var point2 = projection.convertLL2MC(new BMapGL.Point(116.493759,39.914972));
+  
+//   var geometry = new THREE.BoxGeometry(5500, 5500, 5000);
+//   var material =  new THREE.MeshBasicMaterial( { color: 0xff0000, flatShading: true, wireframe: false } );
+//   var cube = new THREE.Mesh(geometry, material);
+//   cube.position.x = point.lng;
+//   cube.position.y = point.lat;
+//   cube.position.z = 2500;
+//   threeLayer.add(cube);
+  
+//   var geometry = new THREE.BoxGeometry(5500, 5500, 5000);
+//   var material =  new THREE.MeshBasicMaterial( { color: 0xffffff, flatShading: true, wireframe: true } );
+//   var cube = new THREE.Mesh(geometry, material);
+//   cube.position.x = point.lng;
+//   cube.position.y = point.lat;
+//   cube.position.z = 2500;
+//   threeLayer.add(cube);
+  
+//   var geometry = new THREE.BoxGeometry(20, 20, 20);
+//   var material =  new THREE.MeshBasicMaterial( { color: 0xff0000, flatShading: true, wireframe: false } );
+//   var cube = new THREE.Mesh(geometry, material);
+//   cube.position.z = 10;
+  
+//   threeLayer.add(cube, point2);
+  }, [])
   return (
-    <div className={styles.wrap}>
+    <div className={styles.wrap} style={{
+      heigh: '100vh'
+    }}>
       <Map
         ref={mapRef}
-        center={{ lng: 116.402544, lat: 39.928216 }}
+        center={{ lng: 118.693328, lat: 33.945154 }}
         zoom="11"
-        styles={{}}
-      >
-        <Marker position={{ lng: 116.402544, lat: 39.928216 }} />
-        {/* <NavigationControl />
-        <InfoWindow
-          position={{ lng: 116.402544, lat: 39.928216 }}
-          text="内容"
-          title="标题"
-        /> */}
-      </Map>
-      <button
-        onClick={() => {
-          console.log(mapRef.current);
+        style={{
+          position: 'relative', height: '100vh'
         }}
-      ></button>
+        mapStyleV2={{     
+          styleJson: ConfigJson
+        }}
+      >
+        <MapvglView effects={['bright']} ref={ref => {
+          console.log('ref.view', ref.view)
+          mapvglView.current = ref.view
+        }}>
+          <MapvglLayer
+            type="ThreeLayer"
+            getMethods={(data) => {
+              console.log('data', data)
+              threeLayer.current = data.setViewport()
+            }}
+          />
+        </MapvglView>
+      </Map>
     </div>
   );
 };
