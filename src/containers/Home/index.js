162d15626f4ea3a73cd289bb4e86a28210563d5e
xulif <1554528002@qq.com>
2022-07-26 18:30:01 +0800

    地图组件

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,33 @@
-import styles from './index.less'
+import styles from "./index.less";
+import { Map, Marker, NavigationControl, InfoWindow } from "react-bmapgl";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const Home = () => {
-	return (
-		<div className={styles.wrap}>
-			12
-		</div>		
-	)
-}
+  const mapRef = useRef(null);
+  return (
+    <div className={styles.wrap}>
+      <Map
+        ref={mapRef}
+        center={{ lng: 116.402544, lat: 39.928216 }}
+        zoom="11"
+        styles={{}}
+      >
+        <Marker position={{ lng: 116.402544, lat: 39.928216 }} />
+        {/* <NavigationControl />
+        <InfoWindow
+          position={{ lng: 116.402544, lat: 39.928216 }}
+          text="内容"
+          title="标题"
+        /> */}
+      </Map>
+      <button
+        onClick={() => {
+          console.log(mapRef.current);
+        }}
+      ></button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
